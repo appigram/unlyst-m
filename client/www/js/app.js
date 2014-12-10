33951fb4e30af0e88f6fca84738f00ea2a682d54
louis <louis.dm.li@gmail.com>
2014-12-10 01:39:03 -0500

    fixed indentation continue indentation is now 0
    fixed indentation continue indentation is now 0

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -7,115 +7,115 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'firebase', 'leaflet-directive'])
 
-  .run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if (window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      }
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
-      }
-    });
+.run(function ($ionicPlatform) {
+  $ionicPlatform.ready(function () {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+    // setup an abstract state for the tabs directive
+  .state('tab', {
+    url: "/tab",
+    abstract: true,
+    templateUrl: "templates/tabs.html"
   })
 
-  .config(function ($stateProvider, $urlRouterProvider) {
+    // Each tab has its own nav history stack:
 
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
-    $stateProvider
-
-      // setup an abstract state for the tabs directive
-      .state('tab', {
-        url: "/tab",
-        abstract: true,
-        templateUrl: "templates/tabs.html"
-      })
-
-      // Each tab has its own nav history stack:
-
-      .state('tab.dash', {
-        url: '/home',
-        views: {
-          'tab-dash': {
-            templateUrl: 'templates/home.html',
-            controller: 'DashCtrl'
-          }
-        }
-      })
-
-      .state('tab.friends', {
-        url: '/friends',
-        views: {
-          'tab-friends': {
-            templateUrl: 'templates/tab-friends.html',
-            controller: 'FriendsCtrl'
-          }
-        }
-      })
-      .state('tab.friend-detail', {
-        url: '/friend/:friendId',
-        views: {
-          'tab-friends': {
-            templateUrl: 'templates/friend-detail.html',
-            controller: 'FriendDetailCtrl'
-          }
-        }
-      })
-
-      .state('tab.account', {
-        url: '/account',
-        views: {
-          'tab-account': {
-            templateUrl: 'templates/tab-account.html',
-            controller: 'AccountCtrl'
-          }
-        }
-      });
-
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/tab/home');
-
-  })
-  .directive('noScroll', function ($document) {
-
-    return {
-      restrict: 'A',
-      link: function ($scope, $element, $attr) {
-
-        $document.on('touchmove', function (e) {
-          e.preventDefault();
-        });
+  .state('tab.dash', {
+    url: '/home',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/home.html',
+        controller: 'DashCtrl'
       }
     }
   })
-  // interceptor for http request. Show loading icon.
-  //.config(function ($httpProvider) {
-  //  $httpProvider.interceptors.push(function ($rootScope) {
-  //    return {
-  //      request: function (config) {
-  //        $rootScope.$broadcast('loading:show')
-  //        return config
-  //      },
-  //      response: function (response) {
-  //        $rootScope.$broadcast('loading:hide')
-  //        return response
-  //      }
-  //    }
-  //  })
-  //})
-  //
-  //.run(function ($rootScope, $ionicLoading) {
-  //  $rootScope.$on('loading:show', function () {
-  //    $ionicLoading.show({template: 'foo'})
-  //  })
-  //
-  //  $rootScope.$on('loading:hide', function () {
-  //    $ionicLoading.hide()
-  //  })
-  //})
+
+  .state('tab.friends', {
+    url: '/friends',
+    views: {
+      'tab-friends': {
+        templateUrl: 'templates/tab-friends.html',
+        controller: 'FriendsCtrl'
+      }
+    }
+  })
+  .state('tab.friend-detail', {
+    url: '/friend/:friendId',
+    views: {
+      'tab-friends': {
+        templateUrl: 'templates/friend-detail.html',
+        controller: 'FriendDetailCtrl'
+      }
+    }
+  })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/home');
+
+})
+.directive('noScroll', function ($document) {
+
+  return {
+    restrict: 'A',
+    link: function ($scope, $element, $attr) {
+
+      $document.on('touchmove', function (e) {
+        e.preventDefault();
+      });
+    }
+  }
+})
+// interceptor for http request. Show loading icon.
+//.config(function ($httpProvider) {
+//  $httpProvider.interceptors.push(function ($rootScope) {
+//    return {
+//      request: function (config) {
+//        $rootScope.$broadcast('loading:show')
+//        return config
+//      },
+//      response: function (response) {
+//        $rootScope.$broadcast('loading:hide')
+//        return response
+//      }
+//    }
+//  })
+//})
+//
+//.run(function ($rootScope, $ionicLoading) {
+//  $rootScope.$on('loading:show', function () {
+//    $ionicLoading.show({template: 'foo'})
+//  })
+//
+//  $rootScope.$on('loading:hide', function () {
+//    $ionicLoading.hide()
+//  })
+//})
 
--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -1,235 +1,235 @@
 angular.module('starter.controllers', ["firebase"])
 
-  .factory('houseDB', ["$firebase", function ($firebase) {
-    var ref = new Firebase("https://fiery-heat-1976.firebaseio.com/unlyst/");
-    var sync = $firebase(ref);
-    return sync.$asArray();
-  }
-  ])
+.factory('houseDB', ["$firebase", function ($firebase) {
+  var ref = new Firebase("https://fiery-heat-1976.firebaseio.com/unlyst/");
+  var sync = $firebase(ref);
+  return sync.$asArray();
+}
+])
 
-  .controller('DashCtrl', function ($scope) {
-  })
+.controller('DashCtrl', function ($scope) {
+})
 
-  .controller('FriendsCtrl', function ($scope, Friends) {
-    $scope.friends = Friends.all();
-  })
+.controller('FriendsCtrl', function ($scope, Friends) {
+  $scope.friends = Friends.all();
+})
 
-  .controller('FriendDetailCtrl', function ($scope, $stateParams, Friends) {
-    $scope.friend = Friends.get($stateParams.friendId);
-  })
+.controller('FriendDetailCtrl', function ($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
 
-  .controller('AccountCtrl', function ($scope) {
-  })
+.controller('AccountCtrl', function ($scope) {
+})
 
-  .controller('HomeCtrl', function ($scope, houseDB, $ionicModal, $ionicSlideBoxDelegate) {
-    $scope.activeSlide = 3;
-    //bind model to scoep; set valuation
-    $scope.home = {};
+.controller('HomeCtrl', function ($scope, houseDB, $ionicModal, $ionicSlideBoxDelegate) {
+  $scope.activeSlide = 3;
+  //bind model to scoep; set valuation
+  $scope.home = {};
 
-    $scope.valuation = $scope.home.valuation;
-    $scope.score = 0;
-    $scope.Math = window.Math;
-    $scope.totalScore = 0;
-    $scope.playCount = 0;
-    $scope.avgScore = 0;
+  $scope.valuation = $scope.home.valuation;
+  $scope.score = 0;
+  $scope.Math = window.Math;
+  $scope.totalScore = 0;
+  $scope.playCount = 0;
+  $scope.avgScore = 0;
 
-    $scope.toronto = {};
-    $scope.markers = {};
-    $scope.layers = {
-      baselayers: {
-        googleRoadmap: {
-          name: 'Google Streets',
-          layerType: 'ROADMAP',
-          type: 'google'
-        }
+  $scope.toronto = {};
+  $scope.markers = {};
+  $scope.layers = {
+    baselayers: {
+      googleRoadmap: {
+        name: 'Google Streets',
+        layerType: 'ROADMAP',
+        type: 'google'
       }
+    }
+  };
+  $scope.defaults = {
+    scrollWheelZoom: false
+  };
+
+
+  //init firebase
+  houseDB.$loaded().then(function () {
+
+    var houses = houseDB;
+    var i = 0;
+    $scope.likes = 20;
+    $scope.imgurl = houses[i].img;
+    $scope.bedRmNum = houses[i].bedRmNum;
+    $scope.bathRmNum = houses[i].bathRmNum;
+    $scope.houseType = houses[i].houseType;
+    $scope.houseSize = houses[i].size;
+    $scope.lotSize = houses[i].landSize;
+    $scope.stories = houses[i].stories;
+    $scope.orientation = houses[i].orientation;
+    $scope.parking = houses[i].parkingNum;
+    $scope.parkingType = houses[i].parkingType;
+    $scope.outdoorSpace = houses[i].outdoorSpace;
+    $scope.buildYr = 2014 - houses[i].buildYr;
+    $scope.address = houses[i].address1;
+    $scope.neighborhood = houses[i].neighborhood;
+    $scope.city = houses[i].city;
+    $scope.hideDetail = true;
+    $scope.expertvalue = houses[i].expertvalue;
+    $scope.crowdvalue = houses[i].crowdvalue;
+    $scope.lat = houses[i].lat;
+    //TODO: change this when houses[i].lng is defined
+    $scope.lng = -79.3711985;
+    $scope.toronto = {
+      lat: $scope.lat,
+      lng: $scope.lng,
+      zoom: 14
     };
-    $scope.defaults = {
-      scrollWheelZoom: false
-    };
-
-
-    //init firebase
-    houseDB.$loaded().then(function () {
-
-      var houses = houseDB;
-      var i = 0;
-      $scope.likes = 20;
-      $scope.imgurl = houses[i].img;
-      $scope.bedRmNum = houses[i].bedRmNum;
-      $scope.bathRmNum = houses[i].bathRmNum;
-      $scope.houseType = houses[i].houseType;
-      $scope.houseSize = houses[i].size;
-      $scope.lotSize = houses[i].landSize;
-      $scope.stories = houses[i].stories;
-      $scope.orientation = houses[i].orientation;
-      $scope.parking = houses[i].parkingNum;
-      $scope.parkingType = houses[i].parkingType;
-      $scope.outdoorSpace = houses[i].outdoorSpace;
-      $scope.buildYr = 2014 - houses[i].buildYr;
-      $scope.address = houses[i].address1;
-      $scope.neighborhood = houses[i].neighborhood;
-      $scope.city = houses[i].city;
-      $scope.hideDetail = true;
-      $scope.expertvalue = houses[i].expertvalue;
-      $scope.crowdvalue = houses[i].crowdvalue;
-      $scope.lat = houses[i].lat;
-      //TODO: change this when houses[i].lng is defined
-      $scope.lng = -79.3711985;
-      $scope.toronto = {
+    $scope.markers = {
+      osloMarker: {
         lat: $scope.lat,
         lng: $scope.lng,
-        zoom: 14
-      };
-      $scope.markers = {
-        osloMarker: {
-          lat: $scope.lat,
-          lng: $scope.lng,
-          //message: "WalkScore:98",
-          focus: true,
-          draggable: false
-        }
-      };
+        //message: "WalkScore:98",
+        focus: true,
+        draggable: false
+      }
+    };
 
-      $ionicModal.fromTemplateUrl('templates/modal.html', function (modal) {
-        $scope.modal = modal;
+    $ionicModal.fromTemplateUrl('templates/modal.html', function (modal) {
+      $scope.modal = modal;
 
-      }, {
-        // Use our scope for the scope of the modal to keep it simple
-        scope: $scope
-        // The animation we want to use for the modal entrance
-        //animation: 'slide-in-up'
-      });
-      $scope.submitScore = function () {
-        $scope.score = 10 - Math.abs(($scope.crowdvalue - $scope.home.valuation)*1.5 / $scope.crowdvalue * 10);
-        if ($scope.score < 0) {
-          $scope.score = 0;
-        }
-        $scope.totalScore += $scope.score;
-        $scope.playCount++;
-        $scope.avgScore = $scope.totalScore / $scope.playCount;
+    }, {
+      // Use our scope for the scope of the modal to keep it simple
+      scope: $scope
+      // The animation we want to use for the modal entrance
+      //animation: 'slide-in-up'
+    });
+    $scope.submitScore = function () {
+      $scope.score = 10 - Math.abs(($scope.crowdvalue - $scope.home.valuation) * 1.5 / $scope.crowdvalue * 10);
+      if ($scope.score < 0) {
+        $scope.score = 0;
+      }
+      $scope.totalScore += $scope.score;
+      $scope.playCount++;
+      $scope.avgScore = $scope.totalScore / $scope.playCount;
 
-      };
+    };
 
-      $scope.next = function () {
-        $ionicSlideBoxDelegate.next();
-      };
-      $scope.previous = function () {
-        $ionicSlideBoxDelegate.previous();
-      };
+    $scope.next = function () {
+      $ionicSlideBoxDelegate.next();
+    };
+    $scope.previous = function () {
+      $ionicSlideBoxDelegate.previous();
+    };
 
-      $scope.activeSlide = 3;
+    $scope.activeSlide = 3;
+    $ionicSlideBoxDelegate.update();
+
+
+    // Called each time the slide changes
+    $scope.slideHasChanged = function (index) {
+      $scope.activeSlide = index;
       $ionicSlideBoxDelegate.update();
+    };
 
-
-      // Called each time the slide changes
-      $scope.slideHasChanged = function (index) {
-        $scope.activeSlide = index;
-        $ionicSlideBoxDelegate.update();
-      };
-
-      $scope.slideToIndex = function (index) {
-        $ionicSlideBoxDelegate.slide(index);
-        $ionicSlideBoxDelegate.update();
-      };
-      $scope.$on('modal.hidden', function () {
-        //$scope.clickNext();
-      });
-
-      $scope.clickNext = function () {
-        setTimeout(function () {
-          //hack: need to call slide twice because images are in ng-repeat's css is not applied.
-          $ionicSlideBoxDelegate.slide(2);
-          $ionicSlideBoxDelegate.slide(3);
-          $ionicSlideBoxDelegate.update();
-        }, 200);
-
-        var length = houses.length;
-        console.log("no: " + i);
-        $scope.hideDetail = true;
-        //need a delay so the next home's value won't be displayed while the modal hides itself
-        //there should a better way to do this
-        setTimeout(function () {
-          //prevent the next score to be shown
-          if (i < length - 1) {
-            i++;
-            $scope.likes = 20;
-            $scope.imgurl = houses[i].img;
-            $scope.bedRmNum = houses[i].bedRmNum;
-            $scope.bathRmNum = houses[i].bathRmNum;
-            $scope.houseType = houses[i].houseType;
-            $scope.houseSize = houses[i].size;
-            $scope.lotSize = houses[i].landSize;
-            $scope.stories = houses[i].stories;
-            $scope.orientation = houses[i].orientation;
-            $scope.parking = houses[i].parkingNum;
-            $scope.parkingType = houses[i].parkingType;
-            $scope.outdoorSpace = houses[i].outdoorSpace;
-            $scope.buildYr = 2014 - houses[i].buildYr;
-            $scope.address = houses[i].address1;
-            $scope.neighborhood = houses[i].neighborhood;
-            $scope.city = houses[i].city;
-            $scope.hideDetail = true;
-            $scope.expertvalue = houses[i].expertvalue;
-            $scope.crowdvalue = houses[i].crowdvalue;
-            $scope.lat = houses[i].lat;
-            $scope.lng = -79.3711985;
-            $scope.toronto = {
-              lat: $scope.lat,
-              lng: $scope.lng,
-              zoom: 14
-            };
-            $scope.markers = {
-              osloMarker: {
-                lat: $scope.lat,
-                lng: $scope.lng,
-                //message: "WalkScore:98",
-                focus: true,
-                draggable: false
-              }
-            };
-          }
-          else {
-            i = 0;
-            $scope.likes = 20;
-            $scope.imgurl = houses[i].img;
-            $scope.bedRmNum = houses[i].bedRmNum;
-            $scope.bathRmNum = houses[i].bathRmNum;
-            $scope.houseType = houses[i].houseType;
-            $scope.houseSize = houses[i].size;
-            $scope.lotSize = houses[i].landSize;
-            $scope.stories = houses[i].stories;
-            $scope.orientation = houses[i].orientation;
-            $scope.parking = houses[i].parkingNum;
-            $scope.parkingType = houses[i].parkingType;
-            $scope.outdoorSpace = houses[i].outdoorSpace;
-            $scope.buildYr = 2014 - houses[i].buildYr;
-            $scope.address = houses[i].address1;
-            $scope.neighborhood = houses[i].neighborhood;
-            $scope.city = houses[i].city;
-            $scope.hideDetail = true;
-            $scope.expertvalue = houses[i].expertvalue;
-            $scope.crowdvalue = houses[i].crowdvalue;
-            $scope.lat = houses[i].lat;
-            $scope.lng = -79.5711985;
-            $scope.toronto = {
-              lat: $scope.lat,
-              lng: $scope.lng,
-              zoom: 14
-            };
-            $scope.markers = {
-              osloMarker: {
-                lat: $scope.lat,
-                lng: $scope.lng,
-                //message: "WalkScore:98",
-                focus: true,
-                draggable: false
-              }
-            };
-          }
-        }, 100);
-
-      };
+    $scope.slideToIndex = function (index) {
+      $ionicSlideBoxDelegate.slide(index);
+      $ionicSlideBoxDelegate.update();
+    };
+    $scope.$on('modal.hidden', function () {
+      //$scope.clickNext();
     });
 
+    $scope.clickNext = function () {
+      setTimeout(function () {
+        //hack: need to call slide twice because images are in ng-repeat's css is not applied.
+        $ionicSlideBoxDelegate.slide(2);
+        $ionicSlideBoxDelegate.slide(3);
+        $ionicSlideBoxDelegate.update();
+      }, 200);
+
+      var length = houses.length;
+      console.log("no: " + i);
+      $scope.hideDetail = true;
+      //need a delay so the next home's value won't be displayed while the modal hides itself
+      //there should a better way to do this
+      setTimeout(function () {
+        //prevent the next score to be shown
+        if (i < length - 1) {
+          i++;
+          $scope.likes = 20;
+          $scope.imgurl = houses[i].img;
+          $scope.bedRmNum = houses[i].bedRmNum;
+          $scope.bathRmNum = houses[i].bathRmNum;
+          $scope.houseType = houses[i].houseType;
+          $scope.houseSize = houses[i].size;
+          $scope.lotSize = houses[i].landSize;
+          $scope.stories = houses[i].stories;
+          $scope.orientation = houses[i].orientation;
+          $scope.parking = houses[i].parkingNum;
+          $scope.parkingType = houses[i].parkingType;
+          $scope.outdoorSpace = houses[i].outdoorSpace;
+          $scope.buildYr = 2014 - houses[i].buildYr;
+          $scope.address = houses[i].address1;
+          $scope.neighborhood = houses[i].neighborhood;
+          $scope.city = houses[i].city;
+          $scope.hideDetail = true;
+          $scope.expertvalue = houses[i].expertvalue;
+          $scope.crowdvalue = houses[i].crowdvalue;
+          $scope.lat = houses[i].lat;
+          $scope.lng = -79.3711985;
+          $scope.toronto = {
+            lat: $scope.lat,
+            lng: $scope.lng,
+            zoom: 14
+          };
+          $scope.markers = {
+            osloMarker: {
+              lat: $scope.lat,
+              lng: $scope.lng,
+              //message: "WalkScore:98",
+              focus: true,
+              draggable: false
+            }
+          };
+        }
+        else {
+          i = 0;
+          $scope.likes = 20;
+          $scope.imgurl = houses[i].img;
+          $scope.bedRmNum = houses[i].bedRmNum;
+          $scope.bathRmNum = houses[i].bathRmNum;
+          $scope.houseType = houses[i].houseType;
+          $scope.houseSize = houses[i].size;
+          $scope.lotSize = houses[i].landSize;
+          $scope.stories = houses[i].stories;
+          $scope.orientation = houses[i].orientation;
+          $scope.parking = houses[i].parkingNum;
+          $scope.parkingType = houses[i].parkingType;
+          $scope.outdoorSpace = houses[i].outdoorSpace;
+          $scope.buildYr = 2014 - houses[i].buildYr;
+          $scope.address = houses[i].address1;
+          $scope.neighborhood = houses[i].neighborhood;
+          $scope.city = houses[i].city;
+          $scope.hideDetail = true;
+          $scope.expertvalue = houses[i].expertvalue;
+          $scope.crowdvalue = houses[i].crowdvalue;
+          $scope.lat = houses[i].lat;
+          $scope.lng = -79.5711985;
+          $scope.toronto = {
+            lat: $scope.lat,
+            lng: $scope.lng,
+            zoom: 14
+          };
+          $scope.markers = {
+            osloMarker: {
+              lat: $scope.lat,
+              lng: $scope.lng,
+              //message: "WalkScore:98",
+              focus: true,
+              draggable: false
+            }
+          };
+        }
+      }, 100);
+
+    };
   });
+
+});
