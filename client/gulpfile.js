577fbaa062e2ad9f148833715e5c19489ef7bc43
louis <louis.dm.li@gmail.com>
2014-12-10 14:38:03 -0500

    minify css for production
    minify css for production

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -10,60 +10,93 @@ var autoprefixer = require('gulp-autoprefixer');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var changed = require('gulp-changed');
+var preprocess = require('gulp-preprocess');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  clean: [
+    './scss/**/*.scss'
+  ],
+  sass: [
+    '!./scss/production.scss',
+    './scss/**/*.scss'
+  ]
 };
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass','html']);
 
 gulp.task('sass', function (done) {
-  gulp.src('./scss/**/*.scss')
-    .pipe(sass())
-    .pipe(autoprefixer({                  // Autoprefix for target browsers
-      browsers: ['last 2 versions'],
-      cascade: true
-    }))
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({extname: '.min.css'}))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+  gulp.src(paths.sass)
+  .pipe(sass())
+  .pipe(autoprefixer({                  // Autoprefix for target browsers
+    browsers: ['last 2 versions'],
+    cascade: true
+  }))
+  .pipe(gulp.dest('./www/css/'))
+  .on('end', done);
+});
+
+gulp.task('html', function() {
+  gulp.src('./www/index.html')
+  .pipe(preprocess({context: { NODE_ENV: 'development', DEBUG: true}})) //To set environment variables in-line
+  .pipe(rename('production.html'))
+  .pipe(gulp.dest('./www/'))
 });
 
 gulp.task('images', function () {
   return gulp.src('./www/img/**/*')
-    .pipe(changed('./www/img/**/*'))
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      use: [pngquant()]
-    }))
-    .pipe(gulp.dest('./www/img/'));
+  //.pipe(changed('./www/img/**/*')) //changed only works on different directories and identical files
+  .pipe(imagemin({
+    progressive: true,
+    svgoPlugins: [{removeViewBox: false}],
+    use: [pngquant()]
+  }))
+  .pipe(gulp.dest('./www/img/'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.sass, ['sass','html']);
 });
 
 gulp.task('install', ['git-check'], function () {
   return bower.commands.install()
-    .on('log', function (data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
+  .on('log', function (data) {
+    gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+  });
 });
 
 gulp.task('git-check', function (done) {
   if (!sh.which('git')) {
     console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+    '  ' + gutil.colors.red('Git is not installed.'),
+    '\n  Git, the version control system, is required to download Ionic.',
+    '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+    '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
     );
     process.exit(1);
   }
   done();
+});
+
+// Production gulp for minification
+gulp.task('prod', ['prod-sass','html-prod']);
+
+gulp.task('html-prod', function() {
+  gulp.src('./www/index.html')
+  .pipe(preprocess({context: { NODE_ENV: 'production', DEBUG: true}})) //To set environment variables in-line
+  .pipe(rename('production.html'))
+  .pipe(gulp.dest('./www/'))
+});
+
+gulp.task('prod-sass', function () {
+  gulp.src('./scss/production.scss')
+  .pipe(sass())
+  .pipe(autoprefixer({                  // Autoprefix for target browsers
+    browsers: ['last 2 versions'],
+    cascade: true
+  }))
+  .pipe(minifyCss({
+    keepSpecialComments: 0
+  }))
+  .pipe(rename({extname: '.min.css'}))
+  .pipe(gulp.dest('./www/production/'));
 });
