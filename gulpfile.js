b36aa3df92d13b663067c2b41f1111ec0fee9ef5
louis <louis.dm.li@gmail.com>
2014-12-10 15:29:54 -0500

    heroku post script
    heroku post script

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var changed = require('gulp-changed');
+var preprocess = require('gulp-preprocess');
+
+var paths = {
+  clean: [
+    './client/scss/**/*.scss'
+  ],
+  sass: [
+    '!./client/scss/production.scss',
+    './client/scss/**/*.scss'
+  ]
+};
+// Production gulp for minification
+gulp.task('heroku:production', ['prod-sass','html-prod']);
+
+gulp.task('html-prod', function() {
+  gulp.src('./client/www/index.html')
+  .pipe(preprocess({context: { NODE_ENV: 'production', DEBUG: true}})) //To set environment variables in-line
+  .pipe(rename('production.html'))
+  .pipe(gulp.dest('./client/www/'))
+});
+
+gulp.task('prod-sass', function () {
+  gulp.src('./client/scss/production.scss')
+  .pipe(sass())
+  .pipe(autoprefixer({                  // Autoprefix for target browsers
+    browsers: ['last 2 versions'],
+    cascade: true
+  }))
+  .pipe(minifyCss({
+    keepSpecialComments: 0
+  }))
+  .pipe(rename({extname: '.min.css'}))
+  .pipe(gulp.dest('./client/www/production/'));
+});
