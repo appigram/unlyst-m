a72fee74d9343f16d017801d2255ff89e083feb8
louis <louis.dm.li@gmail.com>
2015-01-24 04:23:05 -0500

    search ui and updated bower
    search ui and updated bower

--- client/www/src/search/search.js
+++ client/www/src/search/search.js
@@ -2,8 +2,7 @@ starterControllers
 
 .controller('SearchCtrl', function ($scope, $rootScope, fireBaseData, $ionicPopover, $ionicHistory, $state, $mdSidenav) {
   $scope.search = {};
-  $scope.selectedBed = "foo";
-  $scope.bedrooms = ["foo", "bar"];
-  $scope.select = "foo";
-  $scope.list = ["foo", "bar"];
+  $scope.bedrooms = ['1','2','3','4','5+'];
+  $scope.bathrooms = ['1','2','3','4','5+'];
+  $scope.onlyNumbers = /^\d+$/;
 });
